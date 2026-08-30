2d2964f320eca05061e91be3783bab8c0f9f2a8b
Ganesh Shanbhag <ganeshshanbhag.cp@gmail.com>
2026-08-30 11:55:46 +0530

    Update App.jsx

--- skillbridge/src/App.jsx
+++ skillbridge/src/App.jsx
@@ -1,1 +1,19 @@
+import Navbar from "./components/Navbar";
+import Sidebar from "./components/Sidebar";
+import Home from "./pages/Home";
 
+function App() {
+  return (
+    <div className="app">
+      <Navbar />
+
+      <div className="app-body">
+        <Sidebar />
+
+        <Home />
+      </div>
+    </div>
+  );
+}
+
+export default App;
